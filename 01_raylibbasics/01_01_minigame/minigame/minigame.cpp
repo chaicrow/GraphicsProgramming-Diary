aafdc9b4bc8ba301b81e17d8aa4e88b5ccf08590
chaicrow <klara.mvoss@gmail.com>
2026-08-11 11:34:42 +0200

    - lists created (plum, apple) - collision detection added - removal from list upon collision

--- 01_raylibbasics/01_01_minigame/minigame/minigame.cpp
+++ 01_raylibbasics/01_01_minigame/minigame/minigame.cpp
@@ -5,8 +5,9 @@
 #include <iostream>
 #include <vector>
 #include <raylib.h>
+#include <list>
 
-class Food {
+class Food { // Here a food class is created. I want the x and y values to always be randomized upon spawn. I also want to decide the color for every object of that class.
     public:
     int x;
     int y;
@@ -31,41 +32,58 @@ class Food {
 	}
 
 
+
 };
 
 int main()
 {
 
-	int ballX = 400;
+	int ballX = 400; // Setting up x and y values for the spawn of the movable ball.
 	int ballY = 400;
 
-    int foodCount = 0;
 
-
-	Color pasPink = { 255, 192, 203, 255 };
+	Color pasPink = { 255, 192, 203, 255 }; // Creating colors for the ball and the background.
 	Color mossGreen = { 138, 154, 91, 255 };
 
-    InitWindow(800, 800, "raylib mini game");
+    InitWindow(800, 800, "raylib mini game"); // Initializing the game window.
     SetTargetFPS(60);
 
-	// Spawning 20 plums at random positions on the screen
+
+
+
+	// Spawning 20 plums/apples at random positions on the screen
 
     Food plum(128, 0, 128, 255);
     Food apple(255, 0, 0, 255);
 
-   
-    plum.spawn();
-    apple.spawn();
+    std :: list<Food> plums; // Creating a list for all of our plums and apples
+    std::list<Food> apples; 
+
+    for (int i = 0; i < 10; i ++) { // Adding 10 items to each list (we are adding copies of the objects plum and apple)
+
+        plums.push_back(plum); // push_back() is used to add the item in the brackets to the end of the list.
+        apples.push_back(apple);
+    }
+
+    for (Food& plum : plums) { // For every object plum in the list plums we spawn a plum. 
+        plum.spawn();
+    }
+
+    for (Food& apple : apples) {
+        apple.spawn();
+    }
+
 
 
     //Game Loop
+
     while (!WindowShouldClose()) {
 
 
-		// Movement of the ball with WASD keys
+        // Movement of the ball with WASD keys
 
         if (IsKeyDown(KEY_D)) {
-        
+
             ballX += 3;
         }
 
@@ -84,8 +102,40 @@ int main()
             ballY += 3;
         }
 
-		
-		
+        // Collecting foods
+
+        for (auto it = plums.begin(); it != plums.end(); ) {
+
+            if (CheckCollisionCircles(
+
+                Vector2(ballX, ballY), 20, 
+                Vector2(it->x, it->y), 20)) {
+
+                it = plums.erase(it);
+            }
+
+            else {
+                ++it;
+            }
+        }
+
+
+        for (auto it = apples.begin(); it != apples.end(); ) {
+
+            if (CheckCollisionCircles(
+
+                Vector2(ballX, ballY), 20,
+                Vector2(it->x, it->y), 20)) {
+
+                it = apples.erase(it);
+            }
+
+            else {
+                ++it;
+            }
+        }
+   
+
 		// Drawing of all the elements on the screen
 
         BeginDrawing();
@@ -94,14 +144,17 @@ int main()
 
         DrawCircle(ballX, ballY, 20, mossGreen);
 
-        plum.draw();
+        for (Food& plum : plums) {
+            plum.draw();
+        }
 
-        apple.draw();
+        for (Food& apple : apples) {
+            apple.draw();
+        }
 
         EndDrawing();
 
     }
-
 
 
 
